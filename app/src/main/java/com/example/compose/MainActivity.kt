61b60aa536b0a779395f568d0153a82e473dd39b
Svetlana Dyatlova <sdyatlova@ozon.ru>
2024-12-03 18:48:50 +0300

    second lesson

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,67 +3,73 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            opa()
+            Column() {
+                ListItem("Mimi", "Cat")
+                ListItem("Mimi", "Cat")
+                ListItem("Mimi", "Cat")
+                ListItem("Mimi", "Cat")
+                ListItem("Mimi", "Cat")
+                ListItem("Mimi", "Cat")
+            }
         }
     }
-
 }
 
 @Composable
-private fun opa(){
-        Column(modifier = Modifier
-            .background(Color.LightGray) // фон
-            .fillMaxSize(), // заполнить все
-            verticalArrangement = Arrangement.SpaceAround, // текст расположен относительно друг друга по вертикали
-            horizontalAlignment = Alignment.End) { // как будет текст расположен относительно друг друга по горизонтали
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
-        }
-}
-
-@Preview
-@Composable
-private fun opa1(){
-    Row(modifier = Modifier.fillMaxSize().background(Color.LightGray),
-        horizontalArrangement = Arrangement.SpaceEvenly) {
-        Column(
-            modifier = Modifier.fillMaxHeight(0.5f),
-            verticalArrangement = Arrangement.SpaceAround, // текст расположен относительно друг друга по вертикали
-            horizontalAlignment = Alignment.End
-        ) { // как будет текст расположен относительно друг друга по горизонтали
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
-        }
-        Column(
-            modifier = Modifier.fillMaxHeight(0.5f),
-            verticalArrangement = Arrangement.SpaceAround, // текст расположен относительно друг друга по вертикали
-            horizontalAlignment = Alignment.Start
-        ) { // как будет текст расположен относительно друг друга по горизонтали
-            Text(text = "Hello")
-            Text(text = "Hello")
-            Text(text = "Hello")
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+        Box() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.image),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape)
+                )
+                Column(
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
         }
     }
 }
